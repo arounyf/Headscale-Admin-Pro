0950f1bb2fc986d516355d4d0412e62c5f0d1d0f
chenxudong2020 <872603935@qq.com>
2025-03-30 14:57:02 +0800

    fs

--- rootfs/etc/cont-init.d/00-init.sh
+++ rootfs/etc/cont-init.d/00-init.sh
@@ -1,2 +1,12 @@
 #!/command/with-contenv bash
 set -e
+CONTAINER_DB_DIR="/var/lib/headscale"
+mkdir -p $CONTAINER_DB_DIR
+DATABASE_FILE="$CONTAINER_DB_DIR/db.sqlite"
+if [ ! -f "$DATABASE_FILE" ]; then
+    echo "Initializing database..."
+    python3 -m flask db init
+    python3 -m flask db migrate
+    python3 -m flask db upgrade
+fi
+echo "Database initialized."
