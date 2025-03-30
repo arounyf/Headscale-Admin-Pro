306222e2b51e3b6584bf9dfba6f41d7c5b2b7ab7
chenxudong2020 <872603935@qq.com>
2025-03-30 12:51:10 +0800

    Update entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 set -e
 CONTAINER_DB_DIR="/var/lib/headscale"
-DEFAULT_DATA_PATH="/app/default_data"
 mkdir -p $CONTAINER_DB_DIR
-
-if [ ! -f "$CONTAINER_DB_DIR/db.sqlite" ]; then
-    echo "Copying default data to volume..."
-    cp -r $DEFAULT_DATA_PATH/* $CONTAINER_DB_DIR/
-fi
-
 if [ ! -f "$CONTAINER_DB_DIR/db.sqlite" ]; then
     echo "Initializing database..."
     python3 -m flask db init
