c7951ccd158d1006373aaae7c831ca84861c58de
chenxudong2020 <872603935@qq.com>
2025-03-31 22:23:04 +0800

    Update config.py

--- rootfs/app/config.py
+++ rootfs/app/config.py
@@ -1,18 +1,21 @@
 from datetime import timedelta
 import os
 from sqlalchemy import event
-import html
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:////var/lib/headscale/db.sqlite'
 SECRET_KEY = 'SFhkrGKQL2yB9F' # respose解码
 
-
+def remove_quotes(value):
+    # 去除两端的双引号和单引号
+    value = value.strip('"').strip("'")
+    # 替换字符串内的双引号和单引号为无（或根据需要替换为其他字符）
+    return value.replace('"', '').replace("'", '')
 
 BEARER_TOKEN = ''
 PERMANENT_SESSION_LIFETIME = timedelta(seconds=3600)
-SERVER_HOST = html.unescape(os.getenv('SERVER_HOST', 'http://127.0.0.1:8080'))
-TAILSCALE_UP_URL = html.unescape(os.getenv('TAILSCALE_UP_URL', 'http://192.168.6.5:8080'))
-SERVER_NET = html.unescape(os.getenv('SERVER_NET', 'eth0'))
+SERVER_HOST = remove_quotes(os.getenv('SERVER_HOST', 'http://127.0.0.1:8080'))
+TAILSCALE_UP_URL = remove_quotes(os.getenv('TAILSCALE_UP_URL', 'http://192.168.6.5:8080'))
+SERVER_NET = remove_quotes(os.getenv('SERVER_NET', 'eth0'))
 
 
 
