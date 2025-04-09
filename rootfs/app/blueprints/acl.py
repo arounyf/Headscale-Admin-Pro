d09536d769475aa2220ef14362489b764d7a07a4
chenxudong2020 <872603935@qq.com>
2025-04-09 20:01:58 +0800

    重构acl

--- rootfs/app/blueprints/acl.py
+++ rootfs/app/blueprints/acl.py
@@ -2,9 +2,10 @@ import json
 from flask_login import login_required
 from exts import db
 from login_setup import role_required
-from models import UserModel,  ACLModel
+from models import ACLModel
 from flask import Blueprint,  request
 from utils import reload_headscale,fecth_headscale,set_headscale
+from database import DatabaseManager,ResponseResult
 
 bp = Blueprint("acl", __name__, url_prefix='/api/acl')
 
@@ -16,49 +17,11 @@ res_json = {'code': '', 'data': '', 'msg': ''}
 @login_required
 @role_required("manager")
 def getACL():
-    # print(session)
     page = request.args.get('page', default=1, type=int)  # 默认第 1 页
     per_page = request.args.get('limit', default=10, type=int)  # 默认每页 10 条
-    #
-    # 分页查询
-    # 分页查询
-
-    # 使用 func.strftime 格式化时间字段
-    query = ACLModel.query.with_entities(
-        ACLModel.id,
-        ACLModel.acl,
-        UserModel.name,
-        # 可以添加其他需要的字段
-    ) .join(UserModel, ACLModel.user_id == UserModel.id)
-
-    pagination = query.paginate(page=page, per_page=per_page, error_out=False)
-    acls = pagination.items
-    #
-    # 数据格式化
-    acls_list = [{
-        'id': acl.id,
-        'acl': acl.acl,
-        'userName': acl.name,
-
-    } for acl in acls]
-
-    # 接口返回json数据
-    res_json = {
-        'code': '',
-        'data': '',
-        'msg': '',
-        'count': pagination.total,
-        'totalRow': {
-            'count': len(acls)
-        }
-    }
-
-    res_json['code'], res_json['msg'] = '0', '获取成功'
-    res_json['data'] = acls_list
-
-
-    return res_json
-
+    response = DatabaseManager(db).get_acl(page=page, per_page=per_page) 
+    return response.to_dict()
+     
 
 
 @bp.route('/re_acl', methods=['GET','POST'])
@@ -67,48 +30,54 @@ def getACL():
 def re_acl():
     acl_id = request.form.get('aclId')
     new_acl = request.form.get('newAcl')
-
-
-    res_json['code'], res_json['msg'] = '0', '更新成功'
     try:
         json.loads(new_acl)
     except json.JSONDecodeError:
-        res_json['code'], res_json['msg'] = '1', '解析错误'
-
-
-    if (res_json['code'] == '0'):
-        acl = ACLModel.query.filter_by(id=acl_id).first()
-        acl.acl = new_acl
-        db.session.commit()
-
-    return res_json
+        return ResponseResult(
+            code="1",
+            msg="解析错误",
+            count=0,
+            data=[],
+            totalRow={}
+        )
+    DatabaseManager(db).re_acl(acl_id,new_acl)
+    return ResponseResult(
+            code="0",
+            msg="更新成功",
+            count=0,
+            data=[],
+            totalRow={}
+        )
 
 
 @bp.route('/rewrite_acl', methods=['GET','POST'])
 @login_required
 @role_required("manager")
 def rewrite_acl():
-
     acl_path="/etc/headscale/acl.hujson"
     acls = ACLModel.query.all()
-
     acl_list = [json.loads(acl.acl) for acl in acls]
     acl_data = {
         "acls": acl_list
     }
-    print(acl_data)
     try:
         with open(acl_path, 'w') as f:
             json.dump(acl_data, f, indent=4)
-        res_json['code'], res_json['msg'] = '0', '写入成功'
     except Exception as e:
-        # return f"写入文件时出错: {str(e)}", 500
-        res_json['code'], res_json['msg'] = '0', '写入失败'
-        res_json['data'] = str(e)
-
-
-    res_json['data'] = acl_data
-    return  res_json
+        return ResponseResult(
+            code="1",
+            msg="写入失败",
+            count=0,
+            data=str(e),
+            totalRow={}
+        ) 
+    return ResponseResult(
+            code="0",
+            msg="写入成功",
+            count=0,
+            data=acl_data,
+            totalRow={}
+        ) 
 
 
 @bp.route('/read_acl', methods=['GET','POST'])
@@ -117,14 +86,15 @@ def rewrite_acl():
 def read_acl():
     acl_data=fecth_headscale()
     if acl_data is None:
-        res_json['code'], res_json['msg'] = '1', '获取失败'
-        return res_json
-
-    print(acl_data.get('acls',{}))
-
+        return ResponseResult(
+            code="1",
+            msg="获取失败",
+            count=0,
+            data="",
+            totalRow={}
+        ) 
     html_content = "<table border='1' style='margin:10px;'>"
     html_content += "<tr><th>Action</th><th>Source</th><th>Destination</th></tr>"
-
     for item in acl_data.get('acls',{}):
         action = item['action']
         src = ', '.join(item['src'])
@@ -133,11 +103,13 @@ def read_acl():
 
     html_content += "</table>"
 
-    print(html_content)
-    res_json['data'] = html_content
-    res_json['code'], res_json['msg'] = '0', '读取成功'
-
-    return res_json
+    return ResponseResult(
+            code="0",
+            msg="读取成功",
+            count=0,
+            data=html_content,
+            totalRow={}
+        ) 
 
 
 
---
+++ rootfs/app/blueprints/database.py
@@ -0,0 +1,50 @@
+import json
+from dataclasses import dataclass, asdict
+from exts import db
+from models import UserModel, ACLModel
+from typing import Any, List, Dict,Union
+
+@dataclass
+class ResponseResult:
+    code: str
+    msg: str
+    count: int
+    data: Union[List[Dict[str, Any]], Dict[str, Any], str]
+    totalRow: Dict[str, int]
+
+    def to_dict(self):
+        return asdict(self)
+
+
+
+# 数据库操作层
+class DatabaseManager:
+    def __init__(self, db):
+        self.db = db
+
+    # acl分页查询
+    def get_acl(self, page=1, per_page=10):
+      # 使用分页查询并直接返回字典格式
+      pagination = ACLModel.query.with_entities(
+        ACLModel.id.label('id'),
+        ACLModel.acl.label('acl'),
+        UserModel.name.label('userName')
+      ).join(UserModel, ACLModel.user_id == UserModel.id).paginate(
+        page=page, per_page=per_page, error_out=False
+     )
+      data = [row._asdict() for row in pagination.items]
+      return ResponseResult(
+            code="0",
+            msg="获取成功",
+            count=pagination.total,  # 总记录数
+            data=data,
+            totalRow={"count": len(data)}  # 当前页的记录数
+        )
+    
+    def re_acl(self,acl_id,new_acl):
+        acl = ACLModel.query.filter_by(id=acl_id).first()
+        acl.acl = new_acl
+        db.session.commit()
+
+        
+ 
